e603a2673dce3dc8f4a2fcbded1b7f14727c1f79
Michal Skop <michal.skop@kohovolit.eu>
2022-01-10 17:30:54 +0100

    bash script

--- orp/cron_script.sh
+++ orp/cron_script.sh
@@ -5,3 +5,9 @@ git -C $MYPATH pull
 rm "$MYPATH"orp/ockovani-profese.csv
 wget https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-profese.csv
 mv /tmp/ockovani-profese.csv "$MYPATH"orp/ockovani-profese.csv
+
+/home/michal/dev/anaconda3/bin/python3 "$MYPATH"orp/pivot.py
+git -C $MYPATH add orp/orp.csv
+DATE=$(date -Iseconds)
+git -C $MYPATH commit -m "ORP updated ${DATE}"
+git -C $MYPATH push
