c0aaeb334c2c6dd50af9f97e788fae4ebf71ba92
Michal Skop <michal.skop@kohovolit.eu>
2022-01-12 09:34:13 +0100

    ORP updated 2022-01-12T09:33:34+01:00

--- orp/cron_script.sh
+++ orp/cron_script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd /tmp
 MYPATH="/home/michal/dev/coronavirus/cz-covid-maps/"
-git -C $MYPATH pull
-rm "$MYPATH"orp/ockovani-profese.csv
 wget https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/ockovani-profese.csv
+rm "$MYPATH"orp/ockovani-profese.csv
 mv /tmp/ockovani-profese.csv "$MYPATH"orp/ockovani-profese.csv
 
+git -C $MYPATH pull
 /home/michal/dev/anaconda3/bin/python3 "$MYPATH"orp/pivot.py
 git -C $MYPATH add orp/orp.csv
 DATE=$(date -Iseconds)
