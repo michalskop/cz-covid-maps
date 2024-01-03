5c4cd1d4323776890e349d100d704e48b26f0885
Michal Skop <michal.skop@kohovolit.eu>
2024-01-03 17:07:22 +0100

    basic line script, update

--- v3/lines/line.py
+++ v3/lines/line.py
@@ -5,3 +5,12 @@ import pandas as pd
 localpath = "v3/lines/"
 
 # read data from UZIS
+url = "https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/incidence-7-14-kraje.csv"
+
+df = pd.read_csv(url, delimiter=",")
+
+# pivot table
+pt = pd.pivot_table(df, index='datum',  values='incidence_7', fill_value=0, aggfunc='sum').reset_index()
+
+# save
+pt.to_csv(localpath + "incidence_7.csv", index=False)
